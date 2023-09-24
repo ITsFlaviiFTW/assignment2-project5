3337321a559694542b85bb395f9bbb583bfe5ab6
ITsFlaviiFTW <flaviusporumbiel@yahoo.com>
2023-09-23 22:38:10 -0400

    Pre-release and displaying emails

--- assignment2-project5/Source.cpp
+++ assignment2-project5/Source.cpp
@@ -6,43 +6,68 @@
 struct STUDENT_DATA {
     std::string firstName;
     std::string lastName;
+#ifdef PRE_RELEASE
+    std::string email;
+#endif
 };
 
 int main() {
+    std::vector<STUDENT_DATA> students; // Ensure this is at the top of main.
 
 #ifdef PRE_RELEASE
     std::cout << "Running pre-release version." << std::endl;
+    std::ifstream file("StudentData_Emails.txt");
 #else
     std::cout << "Running standard version." << std::endl;
-#endif
-    std::vector<STUDENT_DATA> students;
     std::ifstream file("StudentData.txt");
+#endif
 
     if (!file.is_open()) {
+#ifdef PRE_RELEASE
+        std::cerr << "Failed to open the StudentData_Emails.txt file." << std::endl;
+#else
         std::cerr << "Failed to open the StudentData.txt file." << std::endl;
+#endif
         return -1;
     }
 
     std::string line;
     while (std::getline(file, line)) {
         STUDENT_DATA student;
+        std::stringstream ss(line);
+        std::string token;
 
-        size_t lastSpace = line.find_last_of(" \t"); // Search for the last space or tab
+#ifdef PRE_RELEASE
+        std::getline(ss, token, ',');
+        student.lastName = token;
+
+        std::getline(ss, token, ',');
+        student.firstName = token;
+
+        std::getline(ss, token, ',');
+        student.email = token;
+#else
+        size_t lastSpace = line.find_last_of(" \t");
         if (lastSpace != std::string::npos) {
             student.firstName = line.substr(lastSpace + 1);
             student.lastName = line.substr(0, lastSpace);
         }
         else {
-            // No space found, use the whole line as the last name
             student.lastName = line;
         }
+#endif
 
         students.push_back(student);
     }
 
+
     // Printing the students for verification
     for (const auto& student : students) {
-        std::cout << student.firstName << " " << student.lastName << std::endl;
+        std::cout << student.firstName << " " << student.lastName;
+#ifdef PRE_RELEASE
+        std::cout << " " << student.email;
+#endif
+        std::cout << std::endl;
     }
     return 1;
 }
